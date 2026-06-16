88d76716767094ebc2ce9c8629426988e34e0bb4
71yvonne <tchwang71@outlook.com>
2026-06-16 17:04:16 +0800

    add

---
+++ 链表/reverse_linklist.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 链表节点
+struct node
+{
+	int val; // 数据域
+	node* next; // 指针域
+	
+	node() : next(nullptr) {}
+};
+
+// 初始化链表，返回头节点指针
+node* init_list()
+{
+	node* head = new(node);
+	return head;
+}
+
+// 头插法建表
+node* create_list(node* head, int a[], int n)
+{
+	for (int i = 0; i < n; i++) {
+		node* newnode = new(node);
+		newnode->val = a[i];
+		newnode->next = head->next;
+		head->next = newnode;
+	}
+	
+	return head;
+}
+
+// 遍历链表
+void print(node* head)
+{
+	node* it = head->next;
+	while (it) {
+		cout << it->val << ' ';
+		it = it->next;
+	}
+	
+	cout << endl;
+}
+
+// 反转链表
+node* reverse_list(node* head)
+{
+	node* it = head->next, *pre = nullptr;
+	delete head;
+	while (it) {
+		node* tmp = it->next;
+		it->next = pre;
+		pre = it;
+		it = tmp;
+	}
+	
+	head = new(node);
+	head->next = pre;
+	
+	return head;
+}
+
+int main()
+{
+	int a[10] = {0, 1, 17, 3, 4, 7, 6, 7, 8, 9};
+	node* head = init_list();
+	head = create_list(head, a, 10);
+	print(head);
+	head = reverse_list(head);
+	print(head);
+	
+	return 0;
+}
